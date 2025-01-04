abaf1fbd8bc451a96e348b3b36061d7472f883ba
fmandia <fmandia1201@gmail.com>
2025-01-04 22:04:42 +0100

    First commit

---
+++ lib/screens/listview1pokemon_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Listview1pokemonScreen extends StatelessWidget {
+   
+  const Listview1pokemonScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('Listview1pokemonScreen'),
+      ),
+    );
+  }
+}
