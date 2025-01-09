aaeff8cce7e7b4e486dba8fd6a1cfdb388a40c51
fmandia <fmandia1201@gmail.com>
2025-01-09 18:17:53 +0100

    cardpokemon_screen

--- lib/screens/cardpokemon_screen.dart
+++ lib/screens/cardpokemon_screen.dart
@@ -12,8 +12,32 @@ class CardpokemonScreen extends StatelessWidget {
           child: Text('Card Pokemon')
         ),
       ),
-      body: Center(
-         child: Text('CardpokemonScreen'),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        children: const[
+          CustomPokemonCardTipo1()
+        ],
+      )
+    );
+  }
+}
+
+class CustomPokemonCardTipo1 extends StatelessWidget {
+  const CustomPokemonCardTipo1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.catching_pokemon_outlined),
+            title: Text('Pokemon'),
+            subtitle: Text('Ullamco et ullamco culpa sint eiusmod eiusmod adipisicing velit id tempor mollit sit sit adipisicing. Eu labore labore do aliqua ex laborum cupidatat fugiat culpa ea ullamco. Consequat laborum labore nisi eu aute reprehenderit excepteur sunt ut dolore in. Labore consectetur laborum minim sit amet. Elit ad velit est ut fugiat ipsum non ad sit id occaecat id. Nulla est ipsum incididunt sit consequat ad sint.'),
+          )
+        ],
       ),
     );
   }
